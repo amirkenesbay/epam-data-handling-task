256bfa8577d32000c9c9aebfac3250d4f3be9aff
amirkenesbay <amirkenesbay@gmail.com>
2022-05-27 11:09:44 +0600

    Added the functionality to search for words in the text

--- src/main/java/com/epam/datahandling/lexis/Word.java
+++ src/main/java/com/epam/datahandling/lexis/Word.java
@@ -1,5 +1,7 @@
 package com.epam.datahandling.lexis;
 
+import java.util.Objects;
+
 /**
  * Word is a sequence of characters separated by spaces and punctuation
  */
@@ -8,10 +10,23 @@ public class Word {
     private String content;
 
     public Word(String word) {
-        throw new UnsupportedOperationException("Implement this method");
+        content = word;
     }
 
     public String getContent() {
-        throw new UnsupportedOperationException("Implement this method");
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word = (Word) o;
+        return Objects.equals(content, word.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(content);
     }
 }
