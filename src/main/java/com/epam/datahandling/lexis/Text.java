b3491371abc3663777bda42fd8dbf909adaa82ff
amirkenesbay <amirkenesbay@gmail.com>
2022-05-27 11:10:28 +0600

    Added the functionality to search for words with the regular expressions

--- src/main/java/com/epam/datahandling/lexis/Text.java
+++ src/main/java/com/epam/datahandling/lexis/Text.java
@@ -1,22 +1,35 @@
 package com.epam.datahandling.lexis;
 
+import com.epam.datahandling.utils.RegexProvider;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Text to be parsed
  */
 public class Text {
 
     private String content;
-    private Sentence[] sentences;
+    private static final String SENTENCE_REGULAR_EXPRESSION = "sentences";
 
     public Text(String content) {
-        throw new UnsupportedOperationException("Implement this method");
+        this.content = content;
     }
 
     public String getContent() {
-        throw new UnsupportedOperationException("Implement this method");
+        return content;
     }
 
     public Sentence[] getSentences() {
-        throw new UnsupportedOperationException("Implement this method");
+        Pattern pattern = Pattern.compile(RegexProvider.get(SENTENCE_REGULAR_EXPRESSION));
+        Matcher matcher = pattern.matcher(content);
+        Sentence[] sentences = new Sentence[0];
+        while (matcher.find()) {
+            sentences = Arrays.copyOf(sentences, sentences.length + 1);
+            sentences[sentences.length - 1] = new Sentence(content.substring(matcher.start(), matcher.end()));
+        }
+        return sentences;
     }
 }
