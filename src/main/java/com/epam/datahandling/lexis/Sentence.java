f9f5cda2d78bb3005c81a2a76967aa5af2d1d4e4
amirkenesbay <amirkenesbay@gmail.com>
2022-05-31 11:30:22 +0600

    Added regex for sentences

--- src/main/java/com/epam/datahandling/lexis/Sentence.java
+++ src/main/java/com/epam/datahandling/lexis/Sentence.java
@@ -1,23 +1,51 @@
 package com.epam.datahandling.lexis;
 
+import com.epam.datahandling.utils.RegexProvider;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Sentence is a sequence of characters separated by ".", "!", "?" and new line characters
  */
 public class Sentence {
+    private static final String WORD_REGULAR_EXPRESSION = "word";
 
-    private String content;
-    private Word[] words;
+    private final String content;
 
     public Sentence(String content) {
-        throw new UnsupportedOperationException("Implement this method");
+        this.content = content;
     }
 
     public String getContent() {
-        throw new UnsupportedOperationException("Implement this method");
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sentence sentence = (Sentence) o;
+        return Objects.equals(content, sentence.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(content);
     }
 
     public Word[] getWords() {
-        throw new UnsupportedOperationException("Implement this method");
+        Word[] words;
+        Pattern pattern = Pattern.compile(RegexProvider.get(WORD_REGULAR_EXPRESSION));
+        Matcher matcher = pattern.matcher(content);
+        words = new Word[0];
+        while (matcher.find()) {
+            words = Arrays.copyOf(words, words.length + 1);
+            words[words.length - 1] = new Word(content.substring(matcher.start(), matcher.end()));
+        }
+        return words;
     }
 
 }
